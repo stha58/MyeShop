c22da40eb5ad3aa444f63e0d5ebb82f0782c7574
SthaSuraz <im.suraz58@gmail.com>
2020-03-01 17:52:39 +0545

    Added Product Lists

--- MyeShop/MyeShop.WebUI/Controllers/HomeController.cs
+++ MyeShop/MyeShop.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyeShop.Core.Contracts;
+using MyeShop.Core.Models;
 
 namespace MyeShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        public IRepository<Product> _context;
+        public IRepository<ProductCategory> productCategories;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            _context = productContext;
+            productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = _context.Collection().ToList();
+            return View(products);
         }
 
         public ActionResult About()
@@ -25,6 +36,14 @@ namespace MyeShop.WebUI.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Details(string id)
+        {
+            Product product = _context.Find(id);
+            if (product == null)
+                return HttpNotFound();
+            return View(product);
         }
     }
 }
